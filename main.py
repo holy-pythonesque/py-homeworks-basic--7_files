22bcb9d194b0de3b19f4704d50b6ff48a3a71a6e
holy-pythonesque <89910953+holy-pythonesque@users.noreply.github.com>
2021-10-12 08:03:11 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,31 @@
+from pprint import pprint
+
+def get_data(file_name):
+    data = dict()
+    with open(file_name) as file:
+        for line in file:
+            cookbook = line.strip()
+            counter = int(file.readline())
+
+            temp_list = []
+            for item in range(counter):
+                ingredient, quantity, measure = file.readline().split(' | ')
+                temp_list.append(
+                    {
+                     'ingredient_name': ingredient,
+                     'quantity': quantity,
+                     'measure': measure
+                     }
+                )
+            data[cookbook] = temp_list
+            file.readline()
+    return data
+pprint(get_data('recipes.txt'))
+
+def get_shop_list_by_dishes(dishes, person_count):
+    cookbook = get_data('recipes.txt')
+    dishes, person_count = input()
+        for dish in cookbook:
+            if dish[] == dishes:
+                new_quantity = person_count * cookbook[dishes['quantity']]
+        ...
